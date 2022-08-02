5b75a7da0cf40b56312b3249c199783f1f7d6db6
SahinZambak <sahinzambak7@gmail.com>
2022-08-02 14:27:51 +0300

    2w323

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace fibonacci;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Fibonacci derlemesi için derinlik sayısını giriniz");
+        int sayi = Int32.Parse(Console.ReadLine());
+        Fibonacci(sayi);
+    }
+
+
+    public static void Fibonacci(int len)
+    {
+        int a = 0, b = 1, c = 0;
+        Console.Write("{0} {1}", a, b);
+        for (int i = 2; i < len; i++)
+        {
+            c = a + b;
+            Console.Write(" {0}", c);
+            a = b;
+            b = c;
+        }
+    }
+}
